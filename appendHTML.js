fa6c6afa85dd147a46d5486df5aa5c3a62807803
Markus Lahl <markus.lahl@antwerpes.com>
2024-12-12 11:45:13 +0100

    feat(design): cleanup for collapse functionality

--- appendHTML.js
+++ appendHTML.js
@@ -25,18 +25,21 @@ function getContainerId(day) {
 class ActivityHandler {
     static activityDictionary = new Map()
     static counter = 0;
+    
+    
 
     static pushActivity(day) {
-
         this.counter += 1;
         let divId = day + this.counter.toString();
         let activityElement = new ActivityElement(divId);
         this.activityDictionary.set(divId, activityElement);
-        console.log(this.activityDictionary.size)
 
         let btnId = "Btn" + this.counter.toString();
         let containerId = getContainerId(day);
-        activityElement.appendActivity(containerId, divId, btnId)
+        let textId = day + "Txt" + this.counter.toString();
+        let durId = day + "Dur" + this.counter.toString()
+        
+        activityElement.appendActivity(containerId, divId, btnId, textId, durId)
     }
 
     static removeActivity(activityElement) {
@@ -57,14 +60,14 @@ class ActivityElement {
         this.htmlId = htmlTagId;
     }
 
-    appendActivity(dayId, divId, btnId) {
+    appendActivity(dayId, divId, btnId, textId, durId) {
         document.getElementById(dayId).insertAdjacentHTML("beforeend",
             "           <div id='temp'> \n" +
-            "                <label class=\"ActivityLabel\">\n" +
-            "                    <input class=\"Activity item\" id=\"TuesdayActivity1\" name=\"MondayActivity1\">\n" +
-            "                    <input class=\"Duration item\" id=\"TuesdayDuration1\" name=\"MondayDuration1\">\n" +
-            "                    <button type=\"button\" id='temp2' onclick='' class=\"btn btn-light btn-sm\" \"\n" +
-            "                            style=\"margin-left: 4%; margin-bottom: 0.5%;\">\n" +
+            "                <label class=\"ActivityLabel col-container\">\n" +
+            "                    <input class=\"Activity item\" id='temp2' name='temp2'>\n" +
+            "                    <input class=\"Duration item\" id='temp3' name='temp3'>\n" +
+            "                    <button type=\"button\" id='temp4' onclick='' class=\"btn btn-light btn-sm btnX\" \"\n" +
+            "                            >\n" +
             "                        X\n" +
             "                    </button>\n" +
             "                </label>\n" +
@@ -72,10 +75,15 @@ class ActivityElement {
         )
 
         let activityElement = document.getElementById('temp');
+        let inputElem1 = document.getElementById('temp2');
+        let inputElem2 = document.getElementById('temp3');
+        let button = document.getElementById('temp4');
         activityElement.id = divId;
-        let button = document.getElementById('temp2');
+        inputElem1.id = textId;
+        inputElem1.name = textId;
+        inputElem2.id = durId;
+        inputElem2.name = durId;
         button.id = btnId;
-        //button.addEventListener('click', () => activityElement.remove())
         button.addEventListener('click', () => ActivityHandler.removeActivity(activityElement))
     }
 
